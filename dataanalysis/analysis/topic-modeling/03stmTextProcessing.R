50207864c8f7017e404d2398dcd2913c3a3c7741
VCM admin account <vcm@vcm-12266.vm.duke.edu>
2021-03-09 07:51:05 -0500

    starting full data STM, need to sort out date mismatch upstream

--- dataanalysis/analysis/topic-modeling/03stmTextProcessing.R
+++ dataanalysis/analysis/topic-modeling/03stmTextProcessing.R
@@ -22,19 +22,22 @@ packs2 <- c("stringr", "reshape2",
 packs3 <- c("textclean")
 loadPkg(c(packs, packs2, packs3))
 
-dataPathDesktop <-  "~/Dropbox/WTO/rdatas/"
-dataPath <- "../../"
+## dataPathDesktop <-  "~/Dropbox/WTO/rdatas/"
+dataPath <- "../../../dataprepandclean/"
 savePath <- "./"
 
 ## This has the WTO paragraph-level data,
+## Meetings 01-113
 ## post-processing cleanup
 ## and wealth info
+##
 
-load(paste0(dataPathDesktop,"speakersMetaCleaned.Rdata"))
+speakers.meta <- read.csv(paste0(dataPath,"WTOSpeakerTurnsM1to113.csv"),
+                          stringsAsFactors=FALSE)
 
 colnames(speakers.meta)
 
-dim(speakers.meta)
+dim(speakers.meta) ##8855 x 15
 
 ## Verify no missing data in the columns we'll use:
 sum(is.na(speakers.meta$docid))
@@ -43,28 +46,39 @@ sum(is.na(speakers.meta$date))
 sum(is.na(speakers.meta$cleanedtext))
 sum(is.na(speakers.meta$firstent))
 
+## The one NA is an empty row:
+
+speakers.meta <- na.omit(speakers.meta)
+
+dim(speakers.meta)
+
 #################
 ##### Analysis
 ##################
 
 pageMarkupStopWords <- c("hyperref", "toc", "wtcomtdw",
-                         "pageref") ## have nothing to do with meeting content
+                         "pageref") ## not meeting content
 
 processed <- textProcessor(documents=speakers.meta$cleanedtext,
                            metadata=speakers.meta,
                            removenumbers=TRUE, 
                            customstopwords=pageMarkupStopWords)
 
-summary(processed) ## 8515 documents (paragraphs), 7093  word dictionary
+summary(processed) ## 8853 documents (paragraphs), 7109  word dictionary
 
 out <- prepDocuments(processed$documents,
                      processed$vocab,
                      processed$meta)
 
 ## Convert "date" field into a numeric counter:
+class(out$meta$date)
 
-out$meta$numdate <- as.numeric(out$meta$date)
+head(out$meta$date)
+tail(out$meta$date)
+out$meta$date <- as.Date(out$meta$date,
+                         format="%Y-%m-$d")
 
+summary(out$meta$numdate)
 ## rename objects for ease of reference:
 docs <- out$documents
 vocab <- out$vocab
