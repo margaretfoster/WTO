7c3a5b30cb7243c08da46fc9be385b24ac6d6deb
VCM admin account <vcm@vcm-12266.vm.duke.edu>
2021-03-18 10:21:47 -0400

    exploratory for desktop

--- dataanalysis/analysis/topic-modeling/03bstmExploratorySearchK.R
+++ dataanalysis/analysis/topic-modeling/03bstmExploratorySearchK.R
@@ -36,11 +36,18 @@ if(Sys.info()['user']=="Ergane"){ ## if on my own machine look in Dropbox
 
 load(paste0(dataPathDesktop,"processedTextforSTM.RData"))
 
+ls()
+
+colnames(meta)
+summary(meta$date) ## last date 11/2020
+summary(meta$meetingno) ## goes to 113
+
 #################
 ##### Analysis
 #### Search K over large number of potential topics
 ###########################
-### searchk
+
+### searchk large
 
 colnames(meta)
 
@@ -54,6 +61,19 @@ mod.tdsk <- searchK(documents=docs,
                        data=meta,
                     seed=80620)
 
-save(mod.tdsk,
-     file=paste0(dataPathDesktop, "sK10to100M1to113.Rdata"))
+##save(mod.tdsk,
+##     file=paste0(dataPathDesktop, "sK10to100M1to113.Rdata"))
  
+
+### SearchK for a topic around K=20 that has an "emergencies" topic
+
+KLows=seq(from=20, to=25,by=1)
+
+
+mod.tdsk <- searchK(documents=docs,
+                       vocab=vocab,
+                       K=KLows,
+                    prevalence=~s(numdate)+
+                    as.factor(income_level_iso3c),
+                       data=meta,
+                    seed=80620)
