e4ed2d3a542640f8cc7bc2a53a57fcdd92f9043e
MJF <mjf34@duke.edu>
2021-05-10 15:11:19 -0400

    para-level text update

--- dataanalysis/analysis/topic-modeling/05paraLevelTopicProportions.R
+++ dataanalysis/analysis/topic-modeling/05paraLevelTopicProportions.R
@@ -23,184 +23,93 @@ library(stm)
 
 ## load(paste0(dataPathDesktop, "tradDevParaM1to113-Pandemic.RData"))
 
-load(paste0(dataPathDesktop, "Pairwise/K24-25Model.Rdata"))
+load(paste0(dataPathDesktop, "Pairwise/K24-25Model.Rdata")) ## for the model output
+## load(paste0(dataPathDesktop, "stmM1to113ComparisonK20to25.Rdata")) ## looking for the metadata
 
 ls()
+
+meta <- out$meta
+
+head(meta)
 
 #####################
 ## Summarize output from models
 #####################
-## K21 model:  Look to see if the "vulnerable"
-## economies rises at the end b/c of pandemic
+## K=24 model: the model that we decided to move forward with
 
 ## K85 model: look for rates of pandemic
 ## topic
 
 ######################
-### K= 21
+### K= 24
 #####################
-
-summary(mod.out.21)
-
-## topic 5 is the vulnerable economies
-## some increase, but hard to distinguish from
-## noise at the end of the estimation timeline
-
-plot(prep.21,
-     topics=c(5),
-     "numdate",
-     method="continuous")
-
+attributes(mod.out.24)
+summary(mod.out.24)
 
 ##Extract topics:
 
-theta21Out <- as.data.frame(round(mod.out.21$theta, 4))
+theta24Out <- as.data.frame(round(mod.out.24$theta, 3))  ## round to three places
 
-dim(theta21Out) ## 8553x 21
+dim(theta24Out) ## 8553x 24
 
-head(theta21Out)
+head(theta24Out)
 ## replace "V-" in the column names
-colnames(theta21Out) <- gsub(pattern="V",
-                             replace="M21.Topic",
-                             x=as.character(colnames(theta21Out)))
-theta21Out <- cbind(meta$pid, theta21Out)
+colnames(theta24Out) <- gsub(pattern="V",
+                             replace="M24.Topic",
+                             x=as.character(colnames(theta24Out)))
+theta24Out <- cbind(meta$pid, theta24Out)
 
-head(theta21Out)
+head(theta24Out)
 
-#######################
-## K 85
-#######################
-
-#sink(file="modOut85Summary.txt")
-summary(mod.out.85)
-#sink()
-
-attributes(mod.out.85)
-attributes(mod.out.85$theta) ## 8853 x 85. So this is the estimated topic
-## proportion for each paragraph
-
-theta85Out <- as.data.frame(round(mod.out.85$theta, 4))
-colnames(theta85Out) <- gsub(pattern="V",
-                             replace="M85.Topic",
-                             x=as.character(colnames(theta85Out)))
-theta85Out <- cbind(meta$pid, theta85Out)
-
-head(theta85Out)
 
 ##########
 ### Extract the highest proportion
 ## Attach to the paragraph IDs
 
+#### Assigned paragraph topic in the 24 model:
+theta24Out$max24 <- colnames(theta24Out[,2:22])[apply(
+                                theta24Out[,2:22],1,which.max)] 
 
-### assigned topic in the 85 model:
-theta85Out$max85 <- colnames(theta85Out[,2:86])[apply(
-                                theta85Out[,2:86],1,which.max)] 
+table(theta24Out$max24) ## There is some variation in the assignment of topics
 
-head(theta85Out)
-
-#### Assigned paragraph topic in the 21 model:
-theta21Out$max21 <- colnames(theta21Out[,2:22])[apply(
-                                theta21Out[,2:22],1,which.max)] 
-
-head(theta21Out)
 
 ## send out the paragraph-level topics:
-
-paraTopics <- merge(x= theta21Out[,c("meta$pid", "max21")],
-                    y=theta85Out[,c("meta$pid", "max85")],
-                    by="meta$pid")
-
-
-head(paraTopics)
-tail(paraTopics)
+## will add columns for each topic, which makes a bigger
+## dataframe; but might save some hassle later
 
 paraTopics <- merge(x =meta,
-                    y=paraTopics,
+                    y=theta24Out,
                     by.x="pid",
                     by.y="meta$pid")
 
+dim(paraTopics) ## 8853 x 41
+colnames(paraTopics)
+
+paraTopics$X <- NULL ## No idea what that column is or was
+
 head(paraTopics)
 tail(paraTopics)
 
-##write.csv(paraTopics,
-##          file="M1to113DataAndTopics.csv")
+summary(paraTopics$pid)
+
+write.csv(paraTopics,
+          file=paste0(dataPathDesktop, "M1to113DataAndTopics.csv"))
 
 ###########
-## 85-topic model topic correlations
+## 24-topic model topic correlations
 
-cors.85 <-  topicCorr(mod.out.85,
+cors.24 <-  topicCorr(mod.out.24,
                       method = c("simple"),
-                      cutoff = 0.1,
+                      cutoff = 0.2,
                       verbose = TRUE)
 
 
-class(cors.85)
-attributes(cors.85)
+class(cors.24)
+attributes(cors.24)
 
-plot(cors.85,
+plot(cors.24,
      vertex.size=.75,
      vertex.color="darkblue")
-
-####################
-## Digging into 
-t72 <- findThoughts(mod.out.85,
-                    texts=meta$cleanedtext,
-                    topics=72,
-                    n=3,
-                    meta=meta)
-
-
-plot(t72)
-#### Plotting topics
-########
-attributes(prep.85)
-table(prep.85$data$"income_level_iso3c")
-## ADMN   AGG   HIC   LIC   LMC NOTST   UMC
-
-plot(prep.85,
-     topics=c(26),
-     "numdate",
-     method="continuous",
-     moderator="income_level_iso3c",
-     moderator.value="HIC",
-     printlegend=TRUE)
-
-plot(prep.85,
-     topics=c(26),
-     "numdate",
-     method="continuous",
-     moderator="income_level_iso3c",
-     moderator.value="LIC",
-     printlegend=FALSE,
-     linecol="darkgreen",
-     add=TRUE)
-plot(prep.85,
-     topics=c(26),
-     "numdate",
-     method="continuous",
-     moderator="income_level_iso3c",
-     moderator.value="LMC",
-     linecol="blue",
-     printlegend=FALSE,
-     add=TRUE)
-abline(h=0,lty=4,lwd=1,col="grey45")  # Put a dotted line on t
- 
-plot(prep.85)
-
-#######################
-## K 20 with interaction
-## on time and network activity 
-#######################
-
-load(paste0(dataPathDesktop,
-            "tradDevPara_20InteractDeltType.RData"))
-
-ls()
-
-sink(file="summaryModK20Interact.txt")
-summary(mod.out.20)
-sink()
-
 
 
 
